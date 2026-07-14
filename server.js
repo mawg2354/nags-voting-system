ef7f44999722bac84e68551beb09af6ebafd1f4a
Mohammed Abdul-Wadud Gbeadese <abdulwadudgbe2354@gmail.com>
2026-07-14 21:28:15 +0000

    Add SSL support for Render PostgreSQL

--- server.js
+++ server.js
@@ -31,8 +31,10 @@ const upload = multer({ storage: storage });
 // Connect to database
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 pool.on('error', (err) => {
   console.error('Unexpected error on idle client', err);
   process.exit(-1);
