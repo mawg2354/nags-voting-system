6acaff7ccc3f1d7b4c9c1b909c4768900978bf0b
Mohammed Abdul-Wadud Gbeadese <abdulwadudgbe2354@gmail.com>
2026-07-28 02:55:17 +0000

    Fixed a sytax err

--- server.js
+++ server.js
@@ -249,7 +249,7 @@ app.post('/api/admin/candidates', upload.single('photo'), adminAuth, async (req,
   } catch (err) {
     console.error('Candidate upload error:', err);
     res.status(500).json({ error: 'Failed to create candidate' });
-  };
+  }
 
   
 // DELETE candidate
